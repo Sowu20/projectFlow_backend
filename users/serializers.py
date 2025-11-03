d985aff8652f7d0e8c5ffcda622fcd299e1a50af
Sowu20 <sowukelly67@gmail.com>
2025-11-03 09:37:49 +0000

    backend

--- users/serializers.py
+++ users/serializers.py
@@ -6,7 +6,7 @@ from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model: User
+        model = User
         fields = [
             'id',
             'username',
